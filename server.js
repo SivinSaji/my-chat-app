f05485328e81a3433055a0c6e0e193a0a2949918
Sivin Saji <sivinsaji22@gmail.com>
2022-06-26 23:55:16 +0530

    Added welcome messages

--- server.js
+++ server.js
@@ -2,15 +2,26 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server)
 
-
 //Set static folder
 app.use(express.static(path.join(__dirname,'public')))
 
+
+
+//Run when client connects
+io.on('connection', socket => {
+
+
+    //Welcome current user
+    socket.emit('message',formatMessage(botName,'welcome to ChatApp!'))
+
+
+})
 const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT,() => console.log('Server running on port 3000'));
