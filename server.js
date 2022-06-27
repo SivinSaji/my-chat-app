8bbf8a3bbe4c21e0ee1c71f48fa04c5049e2584f
Sivin Saji <sivinsaji22@gmail.com>
2022-06-27 19:33:56 +0530

    Completed the basic chat application that work as room

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io')
 const formatMessage = require('./utils/messages')
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users')
 
 const app = express();
 const server = http.createServer(app);
@@ -11,17 +12,57 @@ const io = socketio(server)
 //Set static folder
 app.use(express.static(path.join(__dirname,'public')))
 
-
+const botName = 'ChatApp Bot'
 
 //Run when client connects
 io.on('connection', socket => {
 
+    socket.on('joinRoom',({username,room}) => {
+      const user =userJoin(socket.id,username,room);
 
-    //Welcome current user
-    socket.emit('message',formatMessage(botName,'welcome to ChatApp!'))
+      socket.join(user.room);
+
+     //Welcome current user
+     socket.emit('message',formatMessage(botName,'welcome to ChatApp!'))
+
+     //Broadcast when a user connects
+     socket.broadcast.to(user.room).emit('message',formatMessage(botName, user.username+' has joined the chat'))
 
 
+     // Send users and room info
+     io.to(user.room).emit('roomUsers',{
+     room: user.room,
+     users: getRoomUsers(user.room)
+    })
+    })
+
+
+
+    //Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id);
+        console.log(msg);
+    io.to(user.room).emit('message',formatMessage(user.username,msg))
+    })
+
+   //Run when client disconnects
+    socket.on('disconnect', ()=>{
+      const user = userLeave(socket.id);
+      
+      if(user){
+        console.log(user.username);
+        io.to(user.room).emit('message',formatMessage(botName, user.username+' has left the chat'));
+      }
+
+        // Send users and room info
+     io.to(user.room).emit('roomUsers',{
+      room: user.room,
+      users: getRoomUsers(user.room)
+     }) 
+    
+    })
 })
+
 const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT,() => console.log('Server running on port 3000'));
